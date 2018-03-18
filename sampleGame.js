427117ec4207bc5fc32cadc1a776d7c195ac8311
petersensm <petersensm@users.noreply.github.com>
2018-03-17 21:27:25 -0700

    basic player mvmt and interaction
    player walks and jumps
    player stands on ground and falls with gravity
    player interacts with gem
    gem collection triggers portal appearance
    + noise :)

--- sampleGame.js
+++ sampleGame.js
@@ -1,6 +1,11 @@
 // level 1: drawn background, sprite that walks, 
 // and collects gem to access portal
 
+// notes to us: right now, if player touches the gem, it disappears and the portal appears
+// we could have the player collect x # of gems to open the portal or something instead
+// or save that for another level
+// I have also added some lovely and annoying sound for your pleasure
+
 // Variables
 var gems = 0;
 
@@ -12,34 +17,48 @@ ground.setAnimation("greystone_1");
 ground.width = 400;
 
 // player
-var sprite = createSprite(10, 350);
-sprite.setAnimation("alienPink_1");
-sprite.scale = 0.7;
+var player = createSprite(200, 0);
+player.setAnimation("alienPink_1");
+player.scale = 0.7;
 
 // portal
-var portal = createSprite(350, 350);
+// note: for proper interaction need to set collider to circle
+var portal = createSprite(350, 250);
 portal.setAnimation("lollipop_red_1");
 portal.scale = 0.7;
+portal.rotationSpeed = -5;
+portal.visible = false;
+//portal.debug = true;
+portal.setCollider("circle");
 
 // gem
-var gem = createSprite(200, 300);
+// note: for proper interaction need to crop sprite and set collider to circle
+// this will be a good thing for kids to learn
+var gem = createSprite(randomNumber(0,400), randomNumber(0,260));
 gem.setAnimation("ore_emerald_1");
 gem.scale = 0.7;
-
+gem.rotationSpeed = 1;
+//gem.debug = true;
+gem.setCollider("circle");
 
 // Draw Loop
 function draw() {
   // draw the background
   background1();
   // update the sprites
+  playerFall();
+  playerControl();
+  playerLands();
 
   drawSprites();
+  // must have after draw sprites 
+  collectGem();
 }
 
 // Functions
 function background1() {
-background("RosyBrown");
-// asteroid 1
+  background("RosyBrown");
+  // asteroid 1
   noStroke();
   fill("LightPink")
   ellipse(340, 50, 80, 80);
@@ -50,14 +69,44 @@ background("RosyBrown");
   fill("RosyBrown");
   ellipse(300,60,20,20);
   ellipse(366,75,30,30);
-
 }
 
-function background2() {
-
+function playerFall() {
+  player.velocityY = 2;
 }
+
+function playerControl(){
+  if (keyDown("left")) {
+    player.x = player.x - 2;
+    player.setAnimation("alienPink_walk_left");
+  }
+  if (keyDown("right")) {
+    player.x = player.x + 2;
+    player.setAnimation("alienPink_walk_right");
+  }
+  if (keyDown("up")) {
+    player.velocityY = player.velocityY - 20;
+    player.setAnimation("alienPink_1");
+    playSound("sound://category_digital/boing_2.mp3", false);
+  }
+}
+
+function playerLands(){
+  player.collide(ground);
+}
+
+function collectGem() {
+  if (player.isTouching(gem)) {
+    playSound("sound://category_instrumental/trumpet.mp3", false);
+    gem.visible = false;
+    portal.visible = true;
+    
+  }
+  
+}
+
 function tallyGems() {
   fill("white");
   textSize(20);
-  text("Gems: " + score,10, 10, 80, 20);
+  text("Gems: " + gems,10, 10, 80, 20);
 }
