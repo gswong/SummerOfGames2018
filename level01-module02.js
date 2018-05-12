e4239d62f0dd395beb490cf4f3a37faf23a6b408
Gary Wong <gawong@microsoft.com>
2018-05-12 15:24:36 -0700

    fix modules

--- level01-module02.js
+++ level01-module02.js
@@ -1,14 +1,25 @@
-// level 1:
-//   module 1: sprite creation
-//   module 2: player movement and gravity
-//   module 3: create portal for next level
-//   module N: extra features (gem, enemies, ladder climbing, fireballs)
+// level 1: drawn background, sprite that walks, 
+// and collects gem to access portal
+// notes to us: right now, if player touches the gem, it disappears and the portal appears
+// we could have the player collect x # of gems to open the portal or something instead
+// or save that for another level
+// I have also added some lovely and annoying sound for your pleasure
 
 // **** CONFIGS ****
 
+// Change this to false to turn on ruby giving super jumps
+var enableSuperJump = false;
+
 
 // **** VARIABLES ****
+var level = 1;
+var gravity = 1.5;
+var portal;
 
+var playerUpgrades = {
+  superJump: false,
+  // fireballs: false, etc...
+}
 
 // **** SPRITES ****
 
@@ -22,6 +33,32 @@ var player = createSprite(200, 200);
 player.setAnimation("alienPink_1");
 player.scale = 0.7;
 
+// edge sprites: makes a group of edge sprites 
+// - top, bottom, left and right that prevent character from moving off screen
+createEdgeSprites();
+
+
+// gem 
+// note: for proper interaction need to crop sprite and set collider to circle
+// this will be a good thing for kids to learn
+var gem = createSprite(
+  randomNumber(0,400), 
+  randomNumber(15, enableSuperJump ? 160 : 260)
+);
+gem.setAnimation("ore_emerald_1");
+gem.scale = 0.7;
+gem.rotationSpeed = 1;
+//gem.debug = true;
+gem.setCollider("circle");
+
+// ruby (gives you a super jump)
+if (enableSuperJump) {
+  var ruby = createSprite(randomNumber(0,400), randomNumber(200,260));
+  ruby.setAnimation("ore_ruby_1");
+  ruby.scale = 0.5;
+  ruby.rotationSpeed = 1;
+  ruby.setCollider("circle");
+}
 
 // Arrows
 var left = createSprite(20, 375, 30, 20);
@@ -33,16 +70,6 @@ up.setAnimation('up');
 
 // **** FUNCTIONS ****
 
-// Draw Loop
-// remember that order matters
-function drawLevel1() {
-  // draw the background
-  background1();
-
-  // update the sprites
-  drawSprites();
-}
-
 // portal 
 // this is tricky conceptually - create and return
 // note: for proper interaction need to set collider to circle
@@ -51,12 +78,52 @@ function createPortal() {
   portal.setAnimation("lollipop_red_1");
   portal.scale = 0.7;
   portal.rotationSpeed = -5;
+  //portal.debug = true;
   portal.setCollider("circle");
   return portal;
 }
 
+
+// Draw Loop
+// remember that order matters
+function drawLevel1() {
+  // draw the background
+  background1();
+
+  // update the sprites
+  playerGravity();
+  playerControl();
+  playerLands();
+  drawSprites();
+
+  // must have after draw sprites 
+  collectGem();
+  collectRuby();
+  enterPortal();
+}
+
+function drawLevel2() {
+  // draw the background
+  background2();
+
+  // update the sprites
+  playerGravity();
+  playerControl();
+  playerLands();
+  drawSprites();
+
+  // must have after draw sprites 
+  collectGem();
+  enterPortal();
+}
+
 function draw() {
-  drawLevel1();
+  if (level == 1) {
+    drawLevel1();
+  } else if (level == 2) {
+    drawLevel2();
+  }
+  camera.x = player.x;
   moveArrows();
 }
 
@@ -76,9 +143,91 @@ function background1() {
   ellipse(366,75,30,30);
 }
 
+function background2(){
+  background("red");
+}
+
+function playerGravity() {
+  player.velocityY = player.velocityY + gravity;
+}
+
+function playerControl(){
+  var goLeft = 
+    keyDown('left') || mouseIsOver(left);
+  var goRight = 
+    keyDown('right') || mouseIsOver(right);
+  var goUp = 
+    keyDown('up') || mouseIsOver(up);
+  var stopMoving = 
+   keyWentUp("left") 
+   || keyWentUp("right") 
+   || !mouseIsOver(left)
+   || !mouseIsOver(right);
+  
+  
+  if (goLeft && player.velocityX > -10) {
+    player.velocityX = player.velocityX - 1;
+    player.setAnimation("alienPink_walk_left");
+  } else if (goRight && player.velocityX < 10) {
+    player.velocityX = player.velocityX + 1;
+    player.setAnimation("alienPink_walk_right");
+  } else if (stopMoving) {
+    player.setAnimation("alienPink_1");
+  }
+  
+  if (goUp && ground.displace(player)){
+    var jumpVelocity = 
+      enableSuperJump && !playerUpgrades.superJump 
+      ? 20
+      : 30;
+    player.velocityY = player.velocityY - jumpVelocity;
+    player.setAnimation("alienPink_1");
+    playSound("sound://category_digital/boing_2.mp3", false);
+  }
+}
+
+function playerLands(){
+  edges.displace(player);
+  if (ground.displace(player)) {
+    player.friction = 0.2;
+  } else {
+    player.friction = 0;
+  }
+}
+
+// When player collects a gem destroy it and spawn a portal 
+function collectGem() {
+  if (player.isTouching(gem)) {
+    playSound("sound://category_instrumental/trumpet.mp3", false);
+    gem.destroy(); 
+    portal = createPortal();
+  }
+}
+
+// When player collects a ruby destroy it and give the user superJump 
+function collectRuby() {
+  if (ruby && player.isTouching(ruby)) {
+    playSound("sound://category_instrumental/trumpet.mp3", false);
+    ruby.destroy(); 
+    playerUpgrades.superJump = true;
+  }
+}
+
+// When player enters the portal destroy it and advance to next level
+function enterPortal() {
+  if (portal !== undefined) {
+    if (player.isTouching(portal)) {
+      if (player.y < portal.y && player.velocityY > 0) {
+        portal.destroy();
+        playSound("sound://category_instrumental/marimba_upscale_1.mp3", false);
+        level = level + 1; 
+      }
+    }
+  }
+}
+
 function moveArrows() {
   left.x = camera.x - 180;
   right.x = camera.x - 110;
   up.x = camera.x - 145;
 }
-
